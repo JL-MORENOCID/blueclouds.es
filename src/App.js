2588d2f687e8295ac871e83b67ace2e8a5731b5b
JMC <jl.morenocid@gmail.com>
2021-12-15 15:30:39 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import ParallaxCarousel from './components/ParallaxCarousel';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,8 +20,10 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <ParallaxCarousel />
     </div>
-  );
+  )
 }
 
 export default App;
